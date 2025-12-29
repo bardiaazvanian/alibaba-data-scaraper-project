479ab52aca1c8203c142226f1d7bbb76908386b7
bardiaazvanian <38248265+bardiaazvanian@users.noreply.github.com>
2025-12-29 19:47:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -128,8 +128,7 @@ def get_alibaba_price(target_url):
         if driver: driver.quit()
 
 if __name__ == "__main__":
-    url = "https://www.alibaba.ir/international/search/THRALL-DXBALL?adult=1&child=0&infant=0&departing=1404-10-25&flightClass=economy&airlines[0]=W5&pdm=ODU1Nzc0NTQ2NjA2MjM5Mjk3NC8wZTcwMGRkZi0wODQwLTQ3MzgtYjNiYi04NDk3MjA2MWJlNmY=
-"
+    url = "https://www.alibaba.ir/international/search/THRALL-DXBALL?adult=1&child=0&infant=0&departing=1404-10-25&flightClass=economy&airlines[0]=W5&pdm=ODU1Nzc0NTQ2NjA2MjM5Mjk3NC8wZTcwMGRkZi0wODQwLTQ3MzgtYjNiYi04NDk3MjA2MWJlNmY="
     
     print("🚀 Starting Bot...")
     
@@ -156,3 +155,4 @@ if __name__ == "__main__":
     else:
         print("❌ Failed.")
         exit(1)
+
